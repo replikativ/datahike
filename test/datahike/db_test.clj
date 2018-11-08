5865005351f42a947e02c7ad0cd5d59c703b6f4a
Chrislain Razafimahefa <razafima@gmail.com>
2018-11-08 18:22:19 +0100

    Fixes bugs in the tests themselves.

--- test/datahike/db_test.clj
+++ test/datahike/db_test.clj
@@ -10,28 +10,28 @@
 (deftest fdb
   "get"
   (let [db                          (dh-db/empty-db)
-        {:keys [eavt eavt-durable]} (-> (with-datom db (datom 123 :likes "Hans" 0 true))
-                                        (with-datom (datom 124 :likes "GG" 0 true)))]
+        {:keys [eavt eavt-durable]} (-> (with-datom db (datom 123 "likes" "Hans" 0 true))
+                                        (with-datom (datom 124 "likes" "GG" 0 true)))]
 
     (is (== (nth (fdb/get (:eavt-scalable db)
-                          [123 :likes "Hans" 0 true]) 7)
+                          [123 "likes" "Hans" 0 true]) 7)
             123))
     (is (== (nth (fdb/get (:eavt-scalable db)
-                          [124 :likes "GG" 0 true]) 7)
+                          [124 "likes" "GG" 0 true]) 7)
             124)))
 
   "simple range"
   (let [db      (dh-db/empty-db)
         _  (fdb/clear-all)
-        _ (-> (with-datom db (datom 123 :likes "Hans" 0 true))
-              (with-datom (datom 124 :likes "GG" 0 true))
-              (with-datom (datom 125 :likes "GG" 0 true))
-              (with-datom (datom 1 :likes "GG" 0 true))
-              (with-datom (datom 2 :likes "GG" 0 true))
-              (with-datom (datom 3 :likes "GG" 0 true)))]
+        _ (-> (with-datom db (datom 123 "likes" "Hans" 0 true))
+              (with-datom (datom 124 "likes" "GG" 0 true))
+              (with-datom (datom 125 "likes" "GG" 0 true))
+              (with-datom (datom 1 "likes" "GG" 0 true))
+              (with-datom (datom 2 "likes" "GG" 0 true))
+              (with-datom (datom 3 "likes" "GG" 0 true)))]
     (is (= 2
-           (count (fdb/get-range [123 :likes "Hans" 0 true]
-                                 [125 :likes "GG" 0 true]))))
+           (count (fdb/get-range [123 "likes" "Hans" 0 true]
+                                 [125 "likes" "GG" 0 true]))))
     (is (= 2
            (count (fdb/get-range [123]
                                  [125])))))
@@ -39,10 +39,10 @@
   "large range"
   (let [db (dh-db/empty-db)
         _  (fdb/clear-all)
-        _  (reduce #(with-datom %1 (datom %2 :likes "Hans" 0 true)) db (range 100))]
+        _  (reduce #(with-datom %1 (datom %2 "likes" "Hans" 0 true)) db (range 100))]
     (is (= 50
-           (count (fdb/get-range [1 :likes "Hans" 0 true]
-                                 [51 :likes "Hans" 0 true])))))
+           (count (fdb/get-range [1 "likes" "Hans" 0 true]
+                                 [51 "likes" "Hans" 0 true])))))
 
   "large range"
   ;; TODO: if we don't clear the fdb db then tnere are weird things:
@@ -51,15 +51,15 @@
   ;; times and appears multiple times?
   (let [db (dh-db/empty-db)
         ;;        _  (fdb/clear-all)
-        _  (reduce #(with-datom %1 (datom %2 :likes "Hans" 0 true)) db (range 10))]
+        _  (reduce #(with-datom %1 (datom %2 "likes" "Hans" 0 true)) db (range 10))]
     (is (= 2
-           (count (fdb/get-range [1 :likes "Hans" 0 true]
-                                 [3 :likes "Hans" 0 true])))))
+           (count (fdb/get-range [1 "likes" "Hans" 0 true]
+                                 [3 "likes" "Hans" 0 true])))))
 
   "iterate-from"
   (let [db      (dh-db/empty-db)
-        datom-1 (datom 123 :likes "Hans" 0 true)
-        datom-2 (datom 124 :likes "GG" 0 true)
+        datom-1 (datom 123 "likes" "Hans" 0 true)
+        datom-2 (datom 124 "likes" "GG" 0 true)
         datoms  (-> (with-datom db datom-1)
                     (with-datom datom-2))
         iterate #(take % (fdb/iterate-from (fdb/key datom-1)))
@@ -79,21 +79,16 @@
            (seq (nth iterate-5 0))))
     (is (not (= (seq (first iterate-5))
                 (seq (nth iterate-5 1)))))
-    (is (not (= (seq (nth iterate-5 3))
-                (seq (nth iterate-5 4)))))
-    #_(is (= (seq (fdb/key datom))
-             (seq (first iterate-5))))
-    #_(is (= (seq (nth iterate-5 2))
-             (seq (first iterate-5))))
     ))
 
 
 (deftest slice-test
   "slice"
   (let [db                          (dh-db/empty-db)
-        datom-1                     (datom 123 :likes "Hans" 0 true)
-        datom-2                     (datom 124 :likes "GG" 0 true)
-        datom-3                     (datom 125 :likes "GG" 0 true)
+        _                           (fdb/clear-all)
+        datom-1                     (datom 123 "likes" "Hans" 0 true)
+        datom-2                     (datom 124 "likes" "GG" 0 true)
+        datom-3                     (datom 125 "likes" "GG" 0 true)
         {:keys [eavt eavt-durable]} (-> (with-datom db datom-1)
                                         (with-datom datom-2))
         create-eavt                 (fn [e a v tx] (datom e a v tx true))]
@@ -104,6 +99,6 @@
            (first (slice eavt eavt-durable (datom 124 nil nil nil nil) [124]
                          create-eavt))))
     (is (= [datom-1 datom-2]
-           (first (slice eavt eavt-durable (datom 123 nil nil nil nil) [123]
-                         (datom 125 nil nil nil nil)  [125] create-eavt))))
+           (vec (slice eavt eavt-durable (datom 123 nil nil nil nil) [123]
+                       (datom 125 nil nil nil nil)  [125] create-eavt))))
     ))
