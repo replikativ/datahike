aa3ae0c1fe5d378757f6f276919e83cc639ff5ce
Konrad Kühne <konrad.kuehne@lambdaforge.io>
2019-07-25 11:59:43 +0200

    update temporal index example

--- examples/temporal-index/src/examples/core.clj
+++ examples/temporal-index/src/examples/core.clj
@@ -19,29 +19,47 @@
 
 (def config {:uri uri :initial-tx schema-tx})
 
+;; cleanup any previous data
+(d/delete-database uri)
+
 ;; create the database with default configuration w
 (d/create-database config)
 
 ;; connect to the database
 (def conn (d/connect uri))
 
-;; transact some data
-(d/transact! conn [{:name "Alice"
-                    :age 25}
-                   {:name "Bob"
-                    :age 35}
-                   {:name "Charlie"
-                    :age 45
-                    :sibling [[:name "Alice"]]}])
+;; transact age and name data
+(d/transact! conn [{:name "Alice" :age 25} {:name "Bob" :age 30}])
+
+;; let's find name and age of all data
+(def query '[:find ?n ?a :where [?e :name ?n] [?e :age ?a]])
 
 ;; search current data without any new data
-(d/q '[:find ?e ?a ?v :where [?e ?a ?v]] (d/db conn))
+(d/q query (d/db conn))
+
+(def first-date (java.util.Date.))
 
 ;; let's change something
 (d/transact! conn [{:db/id [:name "Alice"] :age 30}])
 
 ;; search for current data of Alice
-(d/q '[:find ?v :in $ ?e :where [?e :age ?v]] (d/db conn) [:name "Alice"])
+(d/q query (d/db conn))
 
 ;; now we search within historical data
-(d/q '[:find ?v :in $ ?e :where [?e :age ?v]] (d/history conn) [:name "Alice"])
+(d/q query (d/history conn))
+
+;; next let's get the current data of a specific time
+(d/q query (d/as-of conn first-date) )
+
+;; now we want to now any additions after a specific time
+(d/q query (d/since conn first-date))
+;; => {}, because :name was transacted before the first date
+
+;; let's build a query where we use the latest db to find the name and the since db to find out who's age changed
+(d/q '[:find ?n ?a
+       :in $ $since
+       :where
+       [$ ?e :name ?n]
+       [$since ?e :age ?a]]
+     (d/db conn)
+     (d/since conn first-date))
