3bf621209edc4282b50d699beb120ff5c6ee2450
Chrislain Razafimahefa <razafima@gmail.com>
2019-08-13 14:09:11 +0200

    Adds an example clj code that we are trying to mimic with java.

--- java/src/datahike/java/test/api/Main.java
+++ java/src/datahike/java/test/api/Main.java
@@ -2,6 +2,22 @@ package datahike.java.test.api;
 
 import datahike.java.api.Api;
 
+// TODO: to-delete
+// (let [uri "datahike:mem://test-empty-db"
+//       _ (d/delete-database uri)
+//       _ (d/create-database uri)
+//       conn (d/connect uri)
+//       db (d/db conn)
+//       tx [{:name "Alice"}]]
+
+//   (is (= {:db/ident {:db/unique :db.unique/identity}} (:schema db)))
+
+//   (testing "transact without schema present"
+//     (is (thrown-msg?
+//          "No schema found in db."
+//          (d/transact! conn tx))))
+
+
 public class Main {
     public static void main(String[] args) {
         String uri = "datahike:mem://test-empty-db";
