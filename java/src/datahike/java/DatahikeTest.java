8e2f9eb77a6d0d8a06e4575e34932500aaa88e4c
Chrislain Razafimahefa <razafima@gmail.com>
2019-08-29 17:11:47 +0200

    Setup junit.

---
+++ java/src/datahike/java/DatahikeTest.java
@@ -0,0 +1,18 @@
+package datahike.java;
+
+import static org.junit.Assert.*;
+
+public class DatahikeTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+    }
+
+    @org.junit.Test
+    public void q() {
+    }
+}
