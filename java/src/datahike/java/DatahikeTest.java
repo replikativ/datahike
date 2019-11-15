cf178e5e8a0f5aa4263111a2b487c00c91c995b8
Chrislain Razafimahefa <razafima@gmail.com>
2019-11-15 12:36:59 +0100

    Fixes indentation.

--- java/src/datahike/java/DatahikeTest.java
+++ java/src/datahike/java/DatahikeTest.java
@@ -221,13 +221,13 @@ public class DatahikeTest {
 
     @Test
     public void with() {
-           Datahike.createDatabase(uri, k(":initial-tx"), schema);
-           conn = Datahike.connect(uri);
+        Datahike.createDatabase(uri, k(":initial-tx"), schema);
+        conn = Datahike.connect(uri);
 
-           PersistentVector txData = (PersistentVector)Clojure.read("[{:db/id 10 :name \"Petr\" :age 44} {:db/id 20 :name \"Ivan\" :age 25} {:db/id 30 :name \"Sergey\" :age 11}]");
-           Object res = Datahike.with(dConn(conn), txData);
-           // TODO: assert. But needs to define an ITxReport first.
-       }
+        PersistentVector txData = (PersistentVector)Clojure.read("[{:db/id 10 :name \"Petr\" :age 44} {:db/id 20 :name \"Ivan\" :age 25} {:db/id 30 :name \"Sergey\" :age 11}]");
+        Object res = Datahike.with(dConn(conn), txData);
+        // TODO: assert. But needs to define an ITxReport first.
+    }
 
     @Test
     public void dbWith() {
