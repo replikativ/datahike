1cd658ed6e51cf51ddc38bec307f3ab81497570e
Judith Massa <j.massa@lambdaforge.io>
2020-02-06 19:31:35 +0100

    Fix datareader problem by datahike update

--- examples/performance/project.clj
+++ examples/performance/project.clj
@@ -4,11 +4,12 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :main performance.core
+  :aot [performance.core]
   :jvm-opts ["-Xmx2g" "-server"]
   :dependencies [[org.clojure/clojure "1.10.0"]
+                 [io.replikativ/datahike "0.2.1"]
                  [com.datomic/datomic-free "0.9.5697"]
                  [incanter/incanter-core "1.9.3"]
                  [incanter/incanter-charts "1.9.3"]
-                 [incanter/incanter-io "1.9.3"]
-                 [io.replikativ/datahike "0.2.0-beta4"]]
+                 [incanter/incanter-io "1.9.3"]]
   :repl-options {:init-ns performance.core})
