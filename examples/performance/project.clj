6eb69139374d8ae67b11fdc2daddce6a642745d5
Konrad Kühne <konrad.kuehne@lambdaforge.io>
2020-03-23 13:36:40 +0100

    re-add performance project

---
+++ examples/performance/project.clj
@@ -0,0 +1,14 @@
+(defproject performance "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :main performance.core
+  :jvm-opts ["-Xmx2g" "-server"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [com.datomic/datomic-free "0.9.5697"]
+                 [incanter/incanter-core "1.9.3"]
+                 [incanter/incanter-charts "1.9.3"]
+                 [incanter/incanter-io "1.9.3"]
+                 [io.replikativ/datahike "0.2.0-beta4"]]
+  :repl-options {:init-ns performance.core})
