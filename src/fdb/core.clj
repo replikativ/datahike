80270d6c1dc9095b2d28206679fddddc6edb8616
chrislain <chrislain@chrislains-macbook-pro.home>
2020-06-09 15:40:21 +0200

    A version which is capable of building keys made of subparts of eavt only. I.e. just [e a] for instance.

--- src/fdb/core.clj
+++ src/fdb/core.clj
@@ -56,10 +56,10 @@
   [index-type [e a v t]]
   ;; TODO: change max-val into min-val
   (cond
-    (= :eavt index-type) (key index-type [e a :max-val 0])
-    (= :aevt index-type) (key index-type [e a :max-val 0])
+    (= :eavt index-type) [e a 0 0]
+    (= :aevt index-type) [e a 0 0]
     ;; TODO: not sure about this case! CHECK IT OUT!
-    (= :avet index-type) (key index-type [:max-val a v 0]))
+    (= :avet index-type) [0 a v 0])
   )
 
 (defn insert
@@ -131,9 +131,9 @@
   "Returns fdb keys in the range [begin end] as a collection of byte-arrays. `begin` and `end` are vectors.
   index-type is `:eavt`, `:aevt` and `:avet`"
   [index-type begin-key end-key]
-  (let [fd        (FDB/selectAPIVersion api-version)
-        b-key (KeySelector/firstGreaterOrEqual begin-key)
-        e-key   (KeySelector/firstGreaterThan end-key
+  (let [fd    (FDB/selectAPIVersion api-version)
+        b-key (KeySelector/firstGreaterOrEqual (key index-type begin-key))
+        e-key   (KeySelector/firstGreaterThan (key index-type end-key)
                     )]
     (with-open [db (.open fd)]
       (tr! db 
@@ -144,12 +144,16 @@
   "Returns vectors in the range [begin end]. `begin` and `end` are vectors *in the [e a v t] form*. But it is really the index-type, i.e., `:eavt`, `:aevt` or `:avet` which sets the semantics of those vectors.
   Additionally, if nils are present in the `begin` vector they are replaced by :min-val to signal the system that we want the min. value at the spot. And conversely for `end` and :max-val."
   [index-type begin end]
-  (let [new-begin (key-part-only index-type begin)
-        new-end (key-part-only index-type end)
+;;(println "*** In get-range: " index-type " -- " begin "----" end)
+  (let [replace-nil (fn [v new-val]
+                      "replace nil in vector v by new-val"
+                      (mapv #(if (nil? %) new-val %) v))
+        new-begin (replace-nil (key-part-only index-type begin) :min-val)
+        new-end (replace-nil (key-part-only index-type end) :max-val)
+  ;;_ (println "*** In get-range: " index-type " -- " new-begin "----" new-end)
         res (get-range-as-byte-array index-type new-begin new-end)
         result (map (partial key->vect index-type) res)]
-    ;; (println "Got from get-range: " (count res))
-(println "*** In get-range: " index-type " -- " new-begin "----" new-end " --- RESULT: " result)
+;;(println "*** In get-range: " index-type " -- " new-begin "----" new-end " --- RESULT: " result)
     result)) 
 
 ;;------------ KeySelectors and iterations
