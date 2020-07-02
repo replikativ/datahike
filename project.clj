fda292ff58786687ed1fef93fd2af13b30bfd10b
Konrad Kühne <konrad.kuehne@protonmail.com>
2020-07-02 17:30:26 +0200

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.replikativ/datahike "0.3.1"
+(defproject io.replikativ/datahike "0.3.2-SNAPSHOT"
   :description "A durable datalog implementation adaptable for distribution."
   :license {:name "Eclipse"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
