1b261635a3b492204ee4638597d5d698b67153ca
Konrad Kühne <konrad.kuehne@lambdaforge.io>
2020-09-30 15:39:30 +0200

    revert dependencies

--- project.clj
+++ project.clj
@@ -5,13 +5,13 @@
   :url "https://github.com/replikativ/datahike"
 
   :dependencies [[org.clojure/clojure       "1.10.1"   :scope "provided"]
-                 [org.clojure/clojurescript "1.10.773" :scope "provided"]
+                 [org.clojure/clojurescript "1.10.597" :scope "provided"]
                  [persistent-sorted-set     "0.1.2"]
                  [org.clojure/tools.reader "1.3.3"]
                  [environ "1.2.0"]
-                 [com.taoensso/timbre "5.0.1"]
-                 [io.replikativ/hitchhiker-tree "0.1.9"]
-                 [io.replikativ/superv.async "0.2.11"]
+                 [com.taoensso/timbre "4.10.0"]
+                 [io.replikativ/hitchhiker-tree "0.1.7"]
+                 [io.replikativ/superv.async "0.2.9"]
                  [io.lambdaforge/datalog-parser "0.1.8"]
                  [io.replikativ/zufall "0.1.0"]
                  [junit/junit "4.13"]]
@@ -88,7 +88,7 @@
              :dev       {:source-paths ["bench/src" "test" "dev"]
                          :dependencies [[org.clojure/tools.nrepl "0.2.13"]
                                         [org.clojure/tools.namespace "1.0.0"]
-                                        [lambdaisland/kaocha "1.0.700"]
+                                        [lambdaisland/kaocha "1.0.632"]
                                         [lambdaisland/kaocha-cljs "0.0-71"]
                                         [io.replikativ/datahike-leveldb "0.1.0"]
                                         [io.replikativ/datahike-postgres "0.3.1-SNAPSHOT"]]}
@@ -96,7 +96,7 @@
                          :source-paths ["benchmark/src" "src"]}
              :aot       {:aot      [#"datahike\.(?!query-v3).*"]
                          :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}
-             :kaocha    {:dependencies [[lambdaisland/kaocha "1.0.700"]
+             :kaocha    {:dependencies [[lambdaisland/kaocha "1.0.632"]
                                         [io.replikativ/datahike-leveldb "0.1.0"]
                                         [io.replikativ/datahike-postgres "0.3.1-SNAPSHOT"]]}}
 
