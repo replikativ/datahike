4da58ccdebce423ab146271cb20d07465a066935
Judith Massa <j.massa@lambdaforge.io>
2021-04-15 13:19:57 +0200

    Add queries

--- benchmark/src/benchmark/config.clj
+++ benchmark/src/benchmark/config.clj
@@ -20,7 +20,13 @@
    [{:db/ident       :s1
      :db/valueType   :db.type/string
      :db/cardinality :db.cardinality/one}
+    {:db/ident       :s2
+     :db/valueType   :db.type/string
+     :db/cardinality :db.cardinality/one}
     {:db/ident       :i1
+     :db/valueType   :db.type/bigint
+     :db/cardinality :db.cardinality/one}
+    {:db/ident       :i2
      :db/valueType   :db.type/bigint
      :db/cardinality :db.cardinality/one}])
 
@@ -28,10 +34,196 @@
    {:s1 (format "%15d" (rand-int max-int))
     :i1 (rand-int max-int)})
 
-(defn q1 [string-val]
-  (conj '[:find ?e :where]
-        (conj '[?e :s1] string-val)))
+(defn rand-int-not-in [int-set]
+  (loop [i (rand-int max-int)]
+    (if (contains? int-set i)
+      i
+      (recur (rand-int max-int)))))
 
-(defn q2 [int-val]
-  (conj '[:find ?a :where [?e :s1 ?a]]
-        (conj '[?e :i1] int-val)))
+(defn rand-str-not-in [str-set]
+  (loop [s (format "%15d" (rand-int max-int))]
+    (if (contains? str-set s)
+      s
+      (recur (rand-int max-int)))))
+
+(defn vec-of [n f]
+  (vec (repeatedly n (f))))
+
+;;; Queries
+
+(defn simple-query [db attr val]
+  {:query (conj '[:find ?e :where]
+                (conj '[?e] attr val))
+   :args [db]})
+
+(defn one-join-query [db attr1 attr2]
+  {:query (conj '[:find ?v1 ?v2 :where]
+                (conj '[?e] attr1 '?v1)
+                (conj '[?e] attr2 '?v2))
+   :args [db]})
+
+(defn one-join-query-first-fixed [db attr1 val1 attr2]
+  {:query (conj '[:find ?v2 :where]
+                (conj '[?e] attr1 val1)
+                (conj '[?e] attr2 '?v2))
+   :args [db]})
+
+(defn one-join-query-second-fixed [db attr1 attr2 val2]
+  {:query (conj '[:find ?v1 :where]
+                (conj '[?e] attr1 '?v1)
+                (conj '[?e] attr2 val2))
+   :args [db]})
+
+(defn scalar-arg-query [db attr val]
+  {:query (conj '[:find ?e
+                  :in $ ?v
+                  :where]
+                (conj '[?e] attr '?v))
+   :args [db val]})
+
+(defn vector-arg-query [db attr vals]
+  {:query (conj '[:find ?e
+                  :in $ ?v
+                  :where]
+                (conj '[?e] attr '?v))
+   :args [db vals]})
+
+(defn less-than-query-1-fixed [db attr comp-val]
+  {:query (conj '[:find ?e :where]
+                (conj '[?e] attr '?v)
+                (conj '[]
+                      (conj '(< ?v) comp-val)))
+   :args [db]})
+
+(defn equals-query-1-fixed [db attr comp-val]
+  {:query (conj '[:find ?e :where]
+                (conj '[?e] attr '?v)
+                (conj '[]
+                      (conj '(= ?v) comp-val)))
+   :args [db]})
+
+
+(defn less-than-query [db attr]
+  {:query (conj '[:find ?e1 ?e2 :where]
+                (conj '[?e1] attr '?v1)
+                (conj '[?e2] attr '?v2)
+                (conj '[]
+                      (conj '(< ?v1 ?v2))))
+   :args [db]})
+
+(defn equals-query [db attr]
+  {:query (conj '[:find ?e1 ?e2 :where]
+                (conj '[?e1] attr '?v1)
+                (conj '[?e2] attr '?v2)
+                (conj '[]
+                      (conj '(= ?v1 ?v2))))
+   :args [db]})
+
+(defn queries [db entities]
+  (let [known-s1 (mapv :s1 entities)
+        known-s2 (mapv :s2 entities)
+        known-i1 (mapv :i1 entities)
+        known-i2 (mapv :i2 entities)
+        known-i1-set (set known-i1)
+        known-i2-set (set known-i2)
+        known-s1-set (set known-s1)
+        known-s2-set (set known-s2)]
+    [{:function :simple-query
+      :query (simple-query db :i1 (rand-nth known-i1))
+      :details {:data-type "int" :data-in-db? true}}
+     {:function :simple-query
+      :query (simple-query db :s1 (rand-nth known-s1))
+      :details {:data-type "str" :data-in-db? true}}
+     {:function :simple-query
+      :query (simple-query db :i1 (rand-int-not-in known-i1-set))
+      :details {:data-type "int" :data-in-db? false}}
+     {:function :simple-query
+      :query (simple-query db :s1 (rand-str-not-in known-i1-set))
+      :details {:data-type "str" :data-in-db? false}}
+
+     {:function :one-join-query
+      :query (one-join-query db :i1 :i2)
+      :details {:data-type "int"}}
+     {:function :one-join-query
+      :query (one-join-query db :s1 :s2)
+      :details {:data-type "int"}}
+
+     {:function :one-join-query-first-fixed
+      :query (one-join-query-first-fixed db :i1 (rand-nth known-i1) :i2)
+      :details {:data-type "int" :data-in-db? true}}
+     {:function :one-join-query-first-fixed
+      :query (one-join-query-first-fixed db :i1 (rand-int-not-in known-i1-set) :i2)
+      :details {:data-type "int" :data-in-db? false}}
+     {:function :one-join-query-first-fixed
+      :query (one-join-query-first-fixed db :s1 (rand-nth known-s1) :s2)
+      :details {:data-type "str" :data-in-db? true}}
+     {:function :one-join-query-first-fixed
+      :query (one-join-query-first-fixed db :s1 (rand-str-not-in known-s1-set) :s2)
+      :details {:data-type "str" :data-in-db? false}}
+
+     {:function :one-join-query-second-fixed
+      :query (one-join-query-second-fixed db :i1 :i2 (rand-nth known-i2))
+      :details {:data-type "int" :data-in-db? true}}
+     {:function :one-join-query-second-fixed
+      :query (one-join-query-second-fixed db :i1 :i2 (rand-int-not-in known-i2-set))
+      :details {:data-type "int" :data-in-db? false}}
+     {:function :one-join-query-second-fixed
+      :query (one-join-query-second-fixed db :s1 :s2 (rand-nth known-s2))
+      :details {:data-type "str" :data-in-db? true}}
+     {:function :one-join-query-second-fixed
+      :query (one-join-query-second-fixed db :s1 :s2 (rand-str-not-in known-s2-set))
+      :details {:data-type "str" :data-in-db? false}}
+
+     {:function :scalar-arg-query
+      :query (scalar-arg-query db :i1 (rand-nth known-i1))
+      :details {:data-type "int" :data-in-db? true}}
+     {:function :scalar-arg-query
+      :query (scalar-arg-query db :i1 (rand-int-not-in known-i1-set))
+      :details {:data-type "int" :data-in-db? true}}
+     {:function :scalar-arg-query
+      :query (scalar-arg-query db :s1 (rand-nth known-s1))
+      :details {:data-type "str" :data-in-db? true}}
+     {:function :scalar-arg-query
+      :query (scalar-arg-query db :s1 (rand-str-not-in known-s1-set))
+      :details {:data-type "str" :data-in-db? true}}
+
+     {:function :vector-arg-query
+      :query (vector-arg-query db :i1 (vec-of 1 #(rand-nth known-i1)))
+      :details {:data-type "int" :data-in-db? true}}
+     {:function :vector-arg-query
+      :query (vector-arg-query db :i1 (vec-of 1 #(rand-int-not-in known-i1-set)))
+      :details {:data-type "int" :data-in-db? true}}
+     {:function :vector-arg-query
+      :query (vector-arg-query db :s1 (vec-of 1 #(rand-nth known-s1)))
+      :details {:data-type "str" :data-in-db? true}}
+     {:function :vector-arg-query
+      :query (vector-arg-query db :s1 (vec-of 1 #(rand-str-not-in known-s1-set)))
+      :details {:data-type "str" :data-in-db? true}}
+
+     {:function :equals-query
+      :query (equals-query db :i1)
+      :details {:data-type "int"}}
+     {:function :equals-query
+      :query (equals-query db :s1)
+      :details {:data-type "str"}}
+
+     {:function :less-than-query
+      :query (less-than-query db :i1)
+      :details {:data-type "int"}}
+     {:function :less-than-query
+      :query (less-than-query db :s1)
+      :details {:data-type "str"}}
+
+     {:function :equals-query-1-fixed
+      :query (equals-query-1-fixed db :i1 (/ max-int 2.0))
+      :details {:data-type "int"}}
+     {:function :equals-query-1-fixed
+      :query (equals-query-1-fixed db :s1 (format "%15d" (/ max-int 2.0)))
+      :details {:data-type "str"}}
+
+     {:function :less-than-query
+      :query (less-than-query-1-fixed db :i1 (/ max-int 2.0))
+      :details {:data-type "int"}}
+     {:function :less-than-query
+      :query (less-than-query-1-fixed db :s1 (format "%15d" (/ max-int 2.0)))
+      :details {:data-type "str"}}]))
--- benchmark/src/benchmark/measure.clj
+++ benchmark/src/benchmark/measure.clj
@@ -27,27 +27,24 @@
 
 (defn measure-performance-full [initial-size n-datoms config]
   (init-db initial-size config)
-  (let [{conn :res t-connection-0 :t} (timed (d/connect config))
+  (let [simple-config (-> config
+                          (assoc :dh-backend (get-in config [:store :backend]))
+                          (dissoc :store))
+        final-size (+ initial-size n-datoms)
+
+        {conn :res t-connection-0 :t} (timed (d/connect config))
         entity-count (int (Math/ceil (/ n-datoms (count c/schema))))
-        tx           (vec (repeatedly entity-count c/rand-entity))
-        t-transaction-n (:t (timed (d/transact conn tx)))
+        entities (vec (repeatedly entity-count c/rand-entity))
+        t-transaction-n (:t (timed (d/transact conn entities)))
 
         _ (d/release conn)
         t-connection-n (:t (timed (d/connect config)))
 
-        rand-s-val (rand-nth (mapv :s1 tx))
-         t-query1-n (:t (timed (d/q (c/q1 rand-s-val) @conn)))
-
-        rand-i-val (rand-nth (mapv :i1 tx))
-        t-query2-n   (:t (timed (d/q (c/q2 rand-i-val) @conn)))
-
-        final-size (+ initial-size n-datoms)
-        simple-config (-> config
-                          (dissoc :store)
-                          (assoc :dh-backend (get-in config [:store :backend])))]
+        queries (vec (for [{:keys [function query details]} (c/queries @conn entities)]
+                         {:time (:t (timed (d/q (:q query) @conn)))
+                          :context {:db-config simple-config :function function :exec-details details :db-size final-size}}))]
     (d/release conn)
-    [{:time t-connection-0  :context {:db-config simple-config :function :connection  :db-size initial-size}}
-     {:time t-transaction-n :context {:db-config simple-config :function :transaction :db-size initial-size :tx-size n-datoms}}
-     {:time t-connection-n  :context {:db-config simple-config :function :connection  :db-size final-size}}
-     {:time t-query1-n      :context {:db-config simple-config :function :query1      :db-size final-size}}
-     {:time t-query2-n      :context {:db-config simple-config :function :query2      :db-size final-size}}]))
+    (conj queries
+          {:time t-connection-0  :context {:db-config simple-config :function :connection  :db-size initial-size}}
+          {:time t-transaction-n :context {:db-config simple-config :function :transaction :db-size initial-size :tx-size n-datoms}}
+          {:time t-connection-n  :context {:db-config simple-config :function :connection  :db-size final-size}})))
