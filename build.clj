98690503ef0e56ee907dd754c733e7de491c88af
Konrad Kühne <kordano@users.noreply.github.com>
2021-12-13 17:04:15 +0100

    Move to main branch (#454)
    * Switch to GitHub Flow and tools.build
    
    This commit changes the CI to create a release on every
    merge to the `main`-branch. The patch-release will always
    incremented when released. CircleCI-pipeline creates
    a release automatically on GitHub and tags the commit.
    
    This commit also switches to use tools.build and with
    that it is possible to run the compilation and jar-builds
    from the REPL. There is no need to run maven anymore.
    
    * Documentation and ADR
    
    Co-authored-by: Timo Kramer <fw-git@timokramer.de>
    Co-authored-by: Timo Kramer <info@lambdaforge.io>

---
+++ build.clj
@@ -0,0 +1,78 @@
+(ns build
+  (:require
+    [borkdude.gh-release-artifact :as gh]
+    [clojure.tools.build.api :as b]
+    [deps-deploy.deps-deploy :as dd]))
+
+(def lib 'io.replikativ/datahike)
+(def version (format "0.4.%s" (b/git-count-revs nil)))
+(def current-commit (gh/current-commit))
+(def class-dir "target/classes")
+(def basis (b/create-basis {:project "deps.edn"}))
+(def jar-file (format "target/%s-%s.jar" (name lib) version))
+
+(defn clean
+  [_]
+  (b/delete {:path "target"}))
+
+(defn compile
+  [_]
+  (b/javac {:src-dirs ["java"]
+            :class-dir class-dir
+            :basis basis
+            :javac-opts ["-source" "8" "-target" "8"]}))
+
+(defn jar
+  [_]
+  (compile nil)
+  (b/write-pom {:class-dir class-dir
+                :src-pom "./template/pom.xml"
+                :lib lib
+                :version version
+                :basis basis
+                :src-dirs ["src"]})
+  (b/copy-dir {:src-dirs ["src" "resources"]
+               :target-dir class-dir})
+  (b/jar {:class-dir class-dir
+          :jar-file jar-file}))
+
+(defn deploy
+  "Don't forget to set CLOJARS_USERNAME and CLOJARS_PASSWORD env vars."
+  [_]
+  (dd/deploy {:installer :remote :artifact jar-file
+              :pom-file (b/pom-path {:lib lib :class-dir class-dir})}))
+
+(defn release
+  [_]
+  (-> (gh/overwrite-asset {:org (namespace lib)
+                           :repo (name lib)
+                           :tag version
+                           :commit current-commit
+                           :file jar-file
+                           :content-type "application/java-archive"})
+      :url
+      println))
+
+(defn install
+  [_]
+  (clean nil)
+  (jar nil)
+  (b/install {:basis (b/create-basis {})
+              :lib lib
+              :version version
+              :jar-file jar-file
+              :class-dir class-dir}))
+
+(comment
+  (b/pom-path {:lib lib :class-dir class-dir})
+  (clean nil)
+  (compile nil)
+  (jar nil)
+  (deploy nil)
+  (release nil)
+  (install nil)
+
+  (name lib)
+  (namespace lib)
+  (require '[babashka.fs :as fs])
+  (fs/file-name (format "target/datahike-%s.jar" version)))
