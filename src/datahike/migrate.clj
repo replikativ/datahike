7b01b68ee183ec2b632ee426669def0721eb8761
Yee Fay Lim <yeefay.lim@gmail.com>
2022-03-03 17:32:02 -0800

    Temporary code of export approaches to simplify benchmarking on remote system

--- src/datahike/migrate.clj
+++ src/datahike/migrate.clj
@@ -1,7 +1,12 @@
 (ns ^:no-doc datahike.migrate
   (:require [datahike.api :as api]
+            [datahike.datom :as d]
             [datahike.db :as db]
-            [clojure.java.io :as io]))
+            [clojure.java.io :as io]
+            [wanderung.datahike :as wd]
+            [tablecloth.api :as tc]))
+
+;; TODO Fressian > Nippy
 
 (defn export-db
   "Export the database in a flat-file of datoms at path."
@@ -10,6 +15,45 @@
               w (io/writer f)]
     (binding [*out* w]
       (doseq [d (datahike.db/-datoms db :eavt [])]
+        (prn d)))))
+
+(defn export-db-wanderung
+  "Export the database in a flat-file of datoms at path."
+  [db path]
+  (with-open [f (io/output-stream path)
+              w (io/writer f)]
+    (binding [*out* w]
+      (doseq [d (let [txs (sort-by first (api/q wd/find-tx-datoms db (java.util.Date. 70)))
+                      query {:query wd/find-datoms-in-tx
+                             :args [(api/history db)]}]
+                  (mapcat (fn [[tid tinst]]
+                            (->> (api/q (update-in query [:args] conj tid))
+                                 (sort-by first)
+                                 (map #(apply d/datom %))
+                                 (into [(d/datom tid :db/txInstant tinst tid true)])))
+                          txs))]
+        (prn d)))))
+
+(defn export-db-tc
+  "Export the database in a flat-file of datoms at path."
+  [db path]
+  (with-open [f (io/output-stream path)
+              w (io/writer f)]
+    (binding [*out* w]
+      (let [datoms (api/datoms (api/history db) :eavt)
+            datoms-table (tc/dataset (map seq datoms)
+                                     {:layout :as-rows
+                                      :column-names [:eid :attr :val :txid :assert]})]
+        (doseq [d (tc/rows (tc/order-by datoms-table [:txid :eid]) :as-seq)]
+          (prn (apply d/datom d)))))))
+
+(defn export-db-clj
+  "Export the database in a flat-file of datoms at path."
+  [db path]
+  (with-open [f (io/output-stream path)
+              w (io/writer f)]
+    (binding [*out* w]
+      (doseq [d (sort-by (juxt d/datom-tx :e) (api/datoms (api/history db) :eavt))]
         (prn d)))))
 
 (defn update-max-tx-from-file
@@ -35,3 +79,62 @@
      (print ".")
      (api/transact conn (vec datoms)))))
 
+(comment
+  (require '[datahike.api :as api]
+           '[datahike.datom :as datom])
+
+  (def schema [{:db/ident       :name
+                :db/cardinality :db.cardinality/one
+                :db/index       true
+                :db/unique      :db.unique/identity
+                :db/valueType   :db.type/string}
+               {:db/ident       :parents
+                :db/cardinality :db.cardinality/many
+                :db/valueType   :db.type/ref}
+               {:db/ident       :age
+                :db/cardinality :db.cardinality/one
+                :db/valueType   :db.type/long}])
+
+  (def export-cfg {:store              {:backend :mem
+                                        :id      "export"}
+                   :keep-history?      true
+                   :schema-flexibility :write
+                   :attribute-refs?    false})
+  (do
+    (api/delete-database export-cfg)
+    (api/create-database export-cfg))
+  (def export-conn (api/connect export-cfg))
+
+  (api/transact export-conn schema)
+  (api/transact export-conn {:tx-data [{:name "Alice" :age  25}
+                                       {:name "Bob" :age  35}
+                                       {:name    "Charlie"
+                                        :age     5
+                                        :parents [[:name "Alice"] [:name "Bob"]]}
+                                       {:name "Daisy" :age 20}
+                                       {:name "Erhard" :age 20}]})
+  (api/transact export-conn {:tx-data [{:name "Erhard" :age 20}]})
+  (api/transact export-conn {:tx-data [[:db/retractEntity [:name "Erhard"]]]})
+  (api/transact export-conn {:tx-data [{:name "Donald" :age 25}]})
+  (api/transact export-conn {:tx-data [{:name "Donald" :age 20}]})
+
+  (export-db-wanderung @export-conn "/tmp/dh.dump")
+  (export-db-tc @export-conn "/tmp/dh.dump")
+  (export-db-clj @export-conn "/tmp/dh.dump")
+
+  (def import-cfg {:store              {:backend :mem
+                                        :id      "import"}
+                   :keep-history?      true
+                   :schema-flexibility :write
+                   :attribute-refs?    false})
+  (do
+    (api/delete-database import-cfg)
+    (api/create-database import-cfg))
+  (def import-conn (api/connect import-cfg))
+  (datahike.migrate/import-db import-conn "/tmp/dh.dump")
+
+  (defn temporal-datoms [db] (api/datoms (api/history db) :eavt))
+  (defn imported-datoms [db] (filter #(< (datom/datom-tx %) (:max-tx db)) (temporal-datoms db)))
+  imported-datoms
+  (= (temporal-datoms @export-conn)
+     (imported-datoms @import-conn)))
