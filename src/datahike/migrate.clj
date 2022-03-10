0fd1fa7727104cb92e557c770ff4db30f71fe2c7
Yee Fay Lim <yeefay.lim@gmail.com>
2022-03-10 15:13:19 -0800

    Export to and import from CBOR files using clj-cbor, for byte array support. Tests to follow.

--- src/datahike/migrate.clj
+++ src/datahike/migrate.clj
@@ -2,11 +2,10 @@
   (:require [datahike.api :as api]
             [datahike.datom :as d]
             [datahike.db :as db]
+            [clj-cbor.core :as cbor]
             [clojure.java.io :as io]
             [wanderung.datahike :as wd]
             [tablecloth.api :as tc]))
-
-;; TODO Fressian > Nippy
 
 (defn export-db
   "Export the database in a flat-file of datoms at path."
@@ -23,16 +22,14 @@
   (with-open [f (io/output-stream path)
               w (io/writer f)]
     (binding [*out* w]
-      (doseq [d (let [txs (sort-by first (api/q wd/find-tx-datoms db (java.util.Date. 70)))
-                      query {:query wd/find-datoms-in-tx
-                             :args [(api/history db)]}]
-                  (mapcat (fn [[tid tinst]]
-                            (->> (api/q (update-in query [:args] conj tid))
-                                 (sort-by first)
-                                 (map #(apply d/datom %))
-                                 (into [(d/datom tid :db/txInstant tinst tid true)])))
-                          txs))]
-        (prn d)))))
+      (let [txs (sort-by first (api/q wd/find-tx-datoms db (java.util.Date. 70)))
+            query {:query wd/find-datoms-in-tx
+                   :args [(api/history db)]}]
+        (cbor/spit-all path (mapcat (fn [[tid tinst]]
+                                      (->> (api/q (update-in query [:args] conj tid))
+                                           (sort-by first)
+                                           (into [[tid :db/txInstant tinst tid true]])))
+                                    txs))))))
 
 (defn export-db-tc
   "Export the database in a flat-file of datoms at path."
@@ -44,8 +41,7 @@
             datoms-table (tc/dataset (map seq datoms)
                                      {:layout :as-rows
                                       :column-names [:eid :attr :val :txid :assert]})]
-        (doseq [d (tc/rows (tc/order-by datoms-table [:txid :eid]) :as-seq)]
-          (prn (apply d/datom d)))))))
+        (cbor/spit-all path (tc/rows (tc/order-by datoms-table [:txid :eid]) :as-seq))))))
 
 (defn export-db-clj
   "Export the database in a flat-file of datoms at path."
@@ -53,31 +49,30 @@
   (with-open [f (io/output-stream path)
               w (io/writer f)]
     (binding [*out* w]
-      (doseq [d (sort-by (juxt d/datom-tx :e) (api/datoms (api/history db) :eavt))]
-        (prn d)))))
+      (cbor/spit-all path (map seq (sort-by (juxt d/datom-tx :e) (api/datoms (api/history db) :eavt)))))))
 
-(defn update-max-tx-from-file
-  "Find bigest tx in file and update max-tx of db.
+(defn update-max-tx
+  "Find bigest tx in datoms and update max-tx of db.
   Note: the last tx might not be the biggest if the db
   has been imported before."
-  [db file]
-  (let [max-tx (->> (line-seq (io/reader file))
-                    (map read-string)
-                    (reduce #(max %1 (nth %2 3)) 0))]
-    (assoc db :max-tx max-tx)))
+  [db datoms]
+  (assoc db :max-tx (reduce #(max %1 (nth %2 3)) 0 datoms)))
+
+(defn- instance-to-date [v]
+  (if (instance? java.time.Instant v) (java.util.Date/from v) v))
 
 (defn import-db
   "Import a flat-file of datoms at path into your database."
   [conn path]
   (println "Preparing import of" path "in batches of 1000")
-  (swap! conn update-max-tx-from-file path)
-  (print "Importing ")
-  (time
-   (doseq  [datoms (->> (line-seq (io/reader path))
-                        (map read-string)
-                        (partition 1000 1000 nil))]
-     (print ".")
-     (api/transact conn (vec datoms)))))
+  (let [datoms (->> (cbor/slurp-all path)
+                    (map #(-> (apply d/datom %) (update :v instance-to-date))))]
+    (swap! conn update-max-tx datoms)
+    (print "Importing ")
+    (time
+     (doseq [partitions (partition 1000 1000 nil datoms)]
+       (print ".")
+       (api/transact conn (vec datoms))))))
 
 (comment
   (require '[datahike.api :as api]
