7b2db796aba461c7d2761a9e257c4561451f4a59
Konrad Kühne <konrad.kuehne@lambdaforge.io>
2022-08-09 18:38:35 +0200

    use cli arguments for versions and folders

--- compat/bin/create_databases.clj
+++ compat/bin/create_databases.clj
@@ -1,17 +1,18 @@
 #!/usr/bin/env bb
 (require '[babashka.process :refer [process check]])
 (require '[clojure.string :as str])
+(require '[babashka.fs :as fs])
 
-(def minimal-minor-version 4)
+(def default-minimal-minor-version "4")
 
-(defn write-with-version [version]
+(defn write-with-version [target-folder version]
   (println "Using version" version)
-  (-> (process ["clj" "-Sdeps" (str "{:deps {io.replikativ/datahike {:mvn/version \"" version "\"}} }") "-X:write-db" ":target-folder" "\"/home/konrad/data/datahike/compat\""] {:out :string})
+  (-> (process ["clj" "-Sdeps" (str "{:deps {io.replikativ/datahike {:mvn/version \"" version "\"}} }") "-X:write-db" ":target-folder" (format "\"%s\"\"" target-folder)] {:out :string})
       check
       :out
       println))
 
-(defn write-versions []
+(defn write-versions [target-folder minimal-minor-version]
   (let [lib "io.replikativ/datahike"
         lines (-> (process ["clj" "-X:deps" "find-versions" ":lib" lib] {:out :string})
                   check
@@ -22,10 +23,16 @@
     (doall
      (doseq [release releases]
        (let [[_ minor patch] (str/split release #"\.")]
-         (if (<= minimal-minor-version (parse-long minor))
+         (if (<= (parse-long minimal-minor-version) (parse-long minor))
            (if-not (contains? ignored-versions patch)
-             (write-with-version release)
+             (write-with-version target-folder release)
              (println "Ignored version:" release))
            (println "Outdated version:" release)))))))
 
-(write-versions)
+(let [[compat-folder minimal-version] *command-line-args*]
+  (if (some? compat-folder)
+    (if (fs/exists? compat-folder)
+      (write-versions compat-folder (or minimal-version default-minimal-minor-version))
+      (prn (format "Folder %s does not exist." compat-folder)))
+    (prn "Please specify the compatibility folder.")))
+
--- compat/bin/read_databases.clj
+++ compat/bin/read_databases.clj
@@ -1,6 +1,7 @@
 #!/usr/bin/env bb
 (require '[babashka.process :refer [process check]])
 (require '[clojure.string :as str])
+(require '[babashka.fs :as fs])
 
 (def minimal-minor-version 4)
 
@@ -12,28 +13,33 @@
       println))
 
 (defn read-with-lib-version [version lib-version]
-  (println (format "Reading version %s with latest release..." version))
+  (println (format "Reading version %s with release %s..." version lib-version))
   (-> (process ["clj" "-Sdeps" (format "{:deps {io.replikativ/datahike {:mvn/version \"%s\"}}}" lib-version) "-X:read-db" ":target-folder" "\"/home/konrad/data/datahike/compat\"" ":version" (format "\"%s\"" version)] {:out :string})
       check
       :out
       println))
 
-(defn read-versions []
-  (let [lib "io.replikativ/datahike"
-        lines (-> (process ["clj" "-X:deps" "find-versions" ":lib" lib] {:out :string})
-                  check
-                  :out
-                  str/split-lines)
-        releases (map (comp :mvn/version read-string) lines)
+(defn read-versions [compat-folder target-version]
+  (let [releases (->> (fs/match compat-folder  "regex:.*\\.export")
+                      (map #(.getName (fs/file %)))
+                      (map #(last (str/split % #"_")))
+                      (map #(str/join "." (butlast (str/split % #"\."))))
+                      sort)
         ignored-versions #{"1497"}]
     (doall
      (doseq [release releases]
-       (let [[_ minor patch] (str/split release #"\.")]
-         (if (<= minimal-minor-version (parse-long minor))
-           (if-not (contains? ignored-versions patch)
-             #_(read-with-latest release)
-             (read-with-lib-version release "0.4.1494")
-             (println "Ignored version:" release))
-           (println "Outdated version:" release)))))))
+       (let [[_ _ patch] (str/split release #"\.")]
+         (if-not (contains? ignored-versions patch)
+           (if (= "latest" target-version)
+             (read-with-latest release)
+             (read-with-lib-version release target-version))
+           (println "Ignored version:" release)))))))
 
-(read-versions)
+(let [[compat-folder target-version] *command-line-args*]
+  (if (some? compat-folder)
+    (if (fs/exists? compat-folder)
+      (if (seq (fs/match compat-folder "regex:.*\\.export"))
+        (read-versions compat-folder (or target-version "latest"))
+        (prn (format "Folder %s does not contain any compat data." compat-folder)))
+      (prn (format "Folder %s does not exist." compat-folder)))
+    (prn "Please specify the compatibility folder.")))
