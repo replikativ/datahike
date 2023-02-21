ef5ac46a4f7b9be66a7067b2dc7dbe0edd877b8e
JMassa <j.massa@lambdaforge.io>
2023-02-21 22:16:52 +0100

    translate compile_libdatahike

--- bb/src/tools/build.clj
+++ bb/src/tools/build.clj
@@ -1,72 +1,111 @@
 (ns tools.build
   (:refer-clojure :exclude [compile])
   (:require [babashka.fs :as fs]
+            [babashka.process :as p]
             [clojure.tools.build.api :as b]
+            [selmer.parser :refer [render]]
             [tools.version :as version :refer [read-edn-file]]))
 
-(defn clean [{:build/keys [target-dir native] :as _config}]
+
+(defn clean [{:keys [target-dir] :as _project-config}]
   (print (str "Cleaning up target directory '" target-dir "'..."))
   (fs/delete-tree target-dir)
-  (when (fs/exists? (:target-dir native))
-    (println (str "Cleaning up native target directory '" (:target-dir native) "'..."))
-    (fs/delete-tree (:target-dir native)))
   (println "Done."))
 
-(defn compile
-  ([] (compile (read-edn-file "config.edn")))
-  ([{:build/keys [deps-file class-dir java-src-dirs] :as _config}]
+(defn basis [{:keys [deps-file aliases] :as _project-config}]
+  (b/create-basis (cond-> {:project deps-file}
+                          aliases (assoc :aliases aliases))))
+
+(defn compile-java
+  ([] (compile-java (read-edn-file "config.edn")))
+  ([{:keys [class-dir java-src-dirs] :as project-config}]
    (print (str "Compiling Java classes saving them to '" class-dir "'..."))
    (b/javac {:src-dirs java-src-dirs
              :class-dir class-dir
-             :basis (b/create-basis {:project deps-file})
-             :javac-opts ["-source" "8" "-target" "8"]})
+             :basis (basis project-config)
+             :javac-opts ["-source" "8" "-target" "8"
+                          "-Xlint:deprecation"]})
    (println "Done.")))
 
-(defn pom-path [{:build/keys [class-dir lib] :as _config}]
+(defn compile-clojure
+    ([] (compile-clojure (read-edn-file "config.edn")))
+    ([{:keys [class-dir src-dirs] :as project-config}]
+     (print (str "Compiling Clojure namespaces saving them to '" class-dir "'..."))
+     (b/compile-clj {:src-dirs src-dirs
+                     :class-dir class-dir 
+                     :basis (basis project-config)})
+     (println "Done.")))
+
+(defn pom-path [{:keys [class-dir lib] :as _project-config}]
   (b/pom-path {:lib lib
                :class-dir class-dir}))
 
-(defn pom [{:build/keys [deps-file class-dir pom-template lib clj-src-dirs scm] :as config}]
+(defn pom [{:keys [pom-template scm] :as repo-config}
+           {:keys [class-dir lib src-dirs] :as project-config}]
   (print (str "Creating pom file from template at '" pom-template "'..."))
-  (b/write-pom {:class-dir class-dir
-                :src-pom pom-template
+  (b/write-pom {:src-pom pom-template
+                :src-dirs src-dirs
+                :class-dir class-dir
                 :lib lib
-                :version (version/string config)
-                :basis (b/create-basis {:project deps-file})
-                :src-dirs clj-src-dirs
+                :version (version/string repo-config)
+                :basis (basis project-config)
                 :scm (assoc scm :tag (version/sha))})
-  (println "Done." "Saved to" (pom-path config)))
+  (println "Done." "Saved to" (pom-path project-config)))
 
-(defn jar-path [{:build/keys [target-dir lib] :as config}]
-  (str target-dir "/" (format "%s-%s.jar" (name lib) (version/string config))))
+(defn jar-path [repo-config {:keys [target-dir jar-pattern] :as project-config}]
+  (str target-dir "/" (render jar-pattern {:project project-config 
+                                           :repo repo-config
+                                           :version-str (version/string repo-config)})))
 
 (defn jar
-  "Returns paths to the jar and the pom file"
-  [{:build/keys [class-dir target-dir clj-src-dirs resource-dirs] :as config}]
+  "Builds jar file"
+  [repo-config {:keys [class-dir target-dir src-dirs resource-dir] :as project-config}]
   (print (str "Packaging jar at '" target-dir "'..."))
-  (b/copy-dir {:src-dirs (concat clj-src-dirs resource-dirs)
+  (b/copy-dir {:src-dirs (filter identity (conj src-dirs resource-dir))
                :target-dir class-dir})
   (b/jar {:class-dir class-dir
-          :jar-file (jar-path config)})
+          :jar-file (jar-path repo-config project-config)})
   (println "Done."))
 
-(defn native-jar-path [{:build/keys [target-dir lib] :as config}]
-  (str target-dir "/" (format "%s-%s-native-shared-library.jar" (name lib) (version/string config))))
-
-(defn native-shared-library [{:build/keys [deps-file class-dir resource-dirs clj-src-dirs native] :as config}]
-  (clean config)
-  (compile config)
-  (b/copy-dir {:src-dirs (vec (concat resource-dirs clj-src-dirs (:source-dirs native)))
+(defn uber
+  "Builds uber jar file"
+  [repo-config {:keys [class-dir target-dir src-dirs resource-dir main] :as project-config}]
+  (print (str "Packaging uber jar at '" target-dir "'..."))
+  (b/copy-dir {:src-dirs (filter identity (conj src-dirs resource-dir))
                :target-dir class-dir})
-  (let [basis (b/create-basis {:project deps-file
-                               :aliases (:aliases native)})]
-    (b/compile-clj {:basis     basis
-                    :src-dirs  clj-src-dirs
-                    :class-dir class-dir})
-    (b/compile-clj {:basis     basis
-                    :src-dirs  (:source-dirs native)
-                    :class-dir class-dir})
-    (b/uber {:class-dir class-dir
-             :uber-file (native-jar-path config)
-             :basis     basis
-             :main      (:main native)})))
+  (b/uber {:class-dir class-dir
+           :uber-file (jar-path repo-config project-config)
+           :basis (basis project-config)
+           :main main})
+  (println "Done."))
+
+(defn native-compile [repo-config {:keys [project-target-dir class-path project-name java-interface] :as project-config}]
+  (if-let [graalvm-dir (System/getenv "GRAALVM_HOME")]
+    (let [native-jar (jar-path repo-config project-config)
+          svm-jar (str graalvm-dir "/lib/svm/builder/svm.jar")]
+      (println "Compiling native bindings Java class.")
+      (p/shell (str graalvm-dir "/bin/javac") "-cp" (str native-jar ":" svm-jar) java-interface)
+      (println "Compiling shared library through native image.")
+      (p/shell (str graalvm-dir "/bin/native-image")
+               "-jar" native-jar
+               "-cp" class-path
+               (str "-H:Name=" project-name)
+               "--shared"
+               "-H:+ReportExceptionStackTraces"
+               "-J-Dclojure.spec.skip-macros=true"
+               "-J-Dclojure.compiler.direct-linking=true"
+               (str "-H:IncludeResources=" (version/string repo-config))
+               "--initialize-at-build-time"
+               "-H:Log=registerResource:"
+               "--verbose"
+               "--no-fallback"
+               "--no-server"
+               "-J-Xmx5g")
+      (fs/delete-tree project-target-dir)
+      (fs/create-dir project-target-dir)
+      (run! #(fs/move % project-target-dir)
+            (concat ["graal_isolate.h" "graal_isolate_dynamic.h"]
+                    (map (fn [ext] (str project-name ext))
+                         [".so" ".h" "_dynamic.h" ".build_artifacts.txt"]))))
+    (do (println "GRAALVM_HOME not set!")
+        (println "Please set GRAALVM_HOME to the root of the graalvm directory on your system."))))
