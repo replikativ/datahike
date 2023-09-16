d7e016401eb0e2584d4e4924c5b3c3745628f795
Timo Kramer <4785848+TimoKramer@users.noreply.github.com>
2023-09-16 14:29:09 +0200

    fix: bencode and cheshire need to be top level (#641)
    * fix: bencode and cheshire need to be in top-level
    
    We need a concept on how to deal with a monorepo and
    separate the dependencies to keep datahike lean.
    
    * fix: #642 prep-lib is broken
    
    * fixup! fix: #642 prep-lib is broken

--- build.clj
+++ build.clj
@@ -1,5 +1,15 @@
 (ns build
-  (:require [clojure.java.shell :refer [sh]]))
+  (:refer-clojure :exclude [compile])
+  (:require [clojure.edn :as edn]
+            [clojure.tools.build.api :as b]))
 
-  (defn bb-trigger [_input]
-    (sh "bb" "jcompile"))
+(def class-dir "target/classes")
+(def basis (b/create-basis {:project "deps.edn"}))
+
+(defn compile-java
+  [_]
+  (b/javac {:src-dirs ["java"]
+            :class-dir class-dir
+            :basis basis
+            :javac-opts ["--release" "8"
+                         "-Xlint:deprecation"]}))
