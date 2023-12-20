19b3f6f045b580626bf987c65f21268eaa79f993
Jonas Östlund <uppfinnarjonas@gmail.com>
2023-12-20 22:50:30 +0100

    FIXUP: Improve the strategy tests

--- test/datahike/test/strategy_test.clj
+++ test/datahike/test/strategy_test.clj
@@ -29,6 +29,7 @@
       (let [[parent-id [[this-type child-count] & remaining-pairs]] tos
             concept-id (id-gen)
             tid (temp-id concept-id)
+            parent-tid (temp-id parent-id)
             edge-tid (format "edge-tmp-%d" counter)]
         (recur (into stack
                      (when (seq remaining-pairs)
@@ -38,17 +39,22 @@
                      [[[:db/add tid :concept/id concept-id]
                        [:db/add tid :concept/type this-type]]
                       (when parent-id
-                        [[:db/add edge-tid :relation/id
+                        [[:db/add tid :concept/broader parent-tid]
+
+                         [:db/add edge-tid :relation/id
                           (relation-id concept-id "broader" parent-id)]
                          [:db/add edge-tid :relation/concept-1
                           tid]
                          [:db/add edge-tid :relation/type "broader"]
                          [:db/add edge-tid :relation/concept-2
-                          (temp-id parent-id)]])])
+                          parent-tid]])])
                (inc counter)
-               (assoc concept-map concept-id {:parent-id parent-id
-                                              :type this-type
-                                              :id concept-id}))))))
+               (cond-> concept-map
+                 true (update concept-id
+                              merge {:parent-id parent-id
+                                     :type this-type
+                                     :id concept-id})
+                 parent-id (update-in [parent-id :child-ids] #(conj (or % []) concept-id))))))))
 (def schema [ ;; Concepts
              #:db{:ident :concept/id,
                   :valueType :db.type/string,
@@ -59,7 +65,11 @@
                   :valueType :db.type/string,
                   :cardinality :db.cardinality/one,
                   :doc "The concepts main type"}
-
+             #:db {:ident :concept/broader
+                   :valueType :db.type/ref
+                   :cardinality :db.cardinality/one
+                   :doc "A broader concept. NOTE: This relation is just used instead of using entities with :relation/type='broader' because it better highlights performance differences in some cases."}
+             
              ;; Relations
              #:db{:ident :relation/id,
                   :valueType :db.type/string,
@@ -148,6 +158,16 @@
                       (edge ?c ?relation-type ?related-c ?r)
                       [?related-c :concept/id ?id]]})
 
+(defn group-concepts-by-type [concept-map]
+  (let [groups (update-vals (group-by (comp :type val)
+                                      concept-map)
+                            (fn [kv-pairs]
+                              (mapv first kv-pairs)))]
+    (doseq [[k v] (sort-by val (update-vals groups count))]
+      (println k v))    
+    (println "Total count:" (count concept-map))
+    groups))
+
 (deftest synthetic-ssyk-tree-test
   (let [conn (initialize-test-db0)
         cgen (concept-id-generator)
@@ -159,10 +179,7 @@
                               ["occupation-name" 0]])
         ssyk-concept-map (:concept-map ssyk-data)
         _ (d/transact conn {:tx-data (:tx-data ssyk-data)})
-        concepts-per-type (update-vals (group-by (comp :type val) ssyk-concept-map)
-                                       (fn [kv-pairs]
-                                         (mapv first kv-pairs)))
-        ;;ssyk-level-2-ids (concepts-per-type "ssyk-level-2")
+        concepts-per-type (group-concepts-by-type ssyk-concept-map)
         ssyk-level-3-ids (concepts-per-type "ssyk-level-3")
         expected-result-fn (fn [concept-ids]
                              (into #{}
@@ -171,11 +188,6 @@
                                            :id (get-in ssyk-concept-map
                                                        [cid :parent-id])}))
                                    concept-ids))]
-    (def the-concepts-per-type concepts-per-type)
-    (doseq [[k v] (sort-by val (update-vals concepts-per-type count))]
-      (println k v))
-    (is (seq ssyk-level-3-ids))
-
     (testing "That the strategies select-simple, select-all and expand-once are more efficient than identiy"
       (let [input-concept-id (first ssyk-level-3-ids)
             _ (is (string? input-concept-id))
@@ -218,9 +230,59 @@
         ;; select-all and expand-once. But it may still be faster than identity, although
         ;; the difference is no longer as remarkable.
         (is (faster-strategy? result-map :select-all :select-simple))
-        (is (faster-strategy? result-map :expand-once :select-simple))))
-    
-    (testing "Find all edges between ssyk-level-2 concepts and ssyk-level-3 concepts. The strategy expand-once seems about twice as fast as select-all"
+        (is (faster-strategy? result-map :expand-once :select-simple))))))
+
+(deftest synthetic-ssyk-tree-test2
+  (let [conn (initialize-test-db0)
+        cgen (concept-id-generator)
+        ssyk-data (make-tree cgen
+                             [["ssyk-level-1" 10]
+                              ["ssyk-level-2" 400]
+                              ["ssyk-level-3" 0]])
+        ssyk-concept-map (:concept-map ssyk-data)
+        _ (d/transact conn {:tx-data (:tx-data ssyk-data)})
+        concepts-per-type (group-concepts-by-type ssyk-concept-map)]
+    (def the-concept-map ssyk-concept-map)
+    (testing "The select-all strategy is expected to perform best because of a small number of possible combinations"
+      (let [parent-ids (take 2 (concepts-per-type "ssyk-level-2"))
+            child-ids (take 3 (get-in ssyk-concept-map [(first parent-ids) :child-ids]))
+            _ (is (= 2 (count parent-ids)))
+            _ (is (= 3 (count child-ids)))
+            result-map (evaluate-strategy-times
+                        (fn [strategy]
+                          (d/q {:query ' {:find [?parent-id ?child-id]
+                                          :keys [parent_id child_id]
+                                          :in [$
+                                               [?parent-id ...]
+                                               [?child-id ...]],
+                                          :where
+                                          [[?pc :concept/id ?parent-id]
+                                           [?cc :concept/id ?child-id]
+                                           [?cc :concept/broader ?pc]]}
+                                :settings {:relprod-strategy strategy}
+                                :args [(d/db conn)
+                                       parent-ids
+                                       child-ids]})))]
+        (println "Find all edges between a few small sets of concept types")
+        (print-strategy-times result-map)
+        (doseq [[_k v] result-map]
+          (is (= 3 (count (:result v)))))
+        (is (faster-strategy? result-map :select-all :expand-once))
+        (is (faster-strategy? result-map :expand-once :select-simple))
+        (is (faster-strategy? result-map :expand-once :identity))))))
+
+(deftest synthetic-ssyk-tree-test3
+  (let [conn (initialize-test-db0)
+        cgen (concept-id-generator)
+        ssyk-data (make-tree cgen
+                             [["ssyk-level-1" 200]
+                              ["ssyk-level-2" 1]
+                              ["ssyk-level-3" 0]])
+        ssyk-concept-map (:concept-map ssyk-data)
+        _ (d/transact conn {:tx-data (:tx-data ssyk-data)})
+        _concepts-per-type (group-concepts-by-type ssyk-concept-map)
+        ]
+    (testing "The select-all strategy will perform poorly because of a large number of possible combinations"
       (let [result-map (evaluate-strategy-times
                         (fn [strategy]
                           (d/q {:query ' {:find [?parent-id ?child-id]
@@ -229,57 +291,13 @@
                                           :where
                                           [[?pc :concept/type "ssyk-level-2"]
                                            [?cc :concept/type "ssyk-level-3"]
-                                           (edge ?cc "broader" ?pc ?r)
+                                           [?cc :concept/broader ?pc]
                                            [?pc :concept/id ?parent-id]
                                            [?cc :concept/id ?child-id]]}
                                 :settings {:relprod-strategy strategy}
-                                :args [(d/db conn)
-                                       rules]})))]
+                                :args [(d/db conn)]})))]
         (doseq [[_k v] result-map]
-          (is (= 64 (count (:result v)))))
+          (is (= 200 (count (:result v)))))
         (println "Find all edges between concept types:")
         (print-strategy-times result-map)
-        (is (faster-strategy? result-map :expand-once :select-all))))
-
-
-    (testing "Find all edges between ssyk-level-2 concepts and ssyk-level-3 concepts and let the edge type be a parameter. Why is expand-once better than select-all?"
-      (let [result-map (evaluate-strategy-times
-                        (fn [strategy]
-                          (d/q {:query ' {:find [?parent-id ?child-id]
-                                          :keys [parent_id child_id]
-                                          :in [$ % [?relation-type ...]],
-                                          :where
-                                          [[?pc :concept/type "ssyk-level-2"]
-                                           [?cc :concept/type "ssyk-level-3"]
-                                           (edge ?cc ?relation-type ?pc ?r)
-                                           [?pc :concept/id ?parent-id]
-                                           [?cc :concept/id ?child-id]]}
-                                :settings {:relprod-strategy strategy}
-                                :args [(d/db conn)
-                                       rules
-                                       #{"broader" "narrower" "related" "close-match"}]})))]
-        (doseq [[_k v] result-map]
-          (is (= 64 (count (:result v)))))
-        (println "Find all edges between concept types with the edge type being parameterized:")
-        (print-strategy-times result-map)
-        (is (faster-strategy? result-map :expand-once :select-all))))
-
-    (testing "WIP: why is expand-once so fast???"
-      (let [result-map (evaluate-strategy-times
-        (fn [strategy]
-          (d/q {:query ' {:find [?parent-id ?child-id]
-                          :keys [parent_id child_id]
-                          :in [$ %
-                               [?parent-id ...]
-                               [?child-id ...]],
-                          :where
-                          [[?pc :concept/id ?parent-id]
-                           [?cc :concept/id ?child-id]
-                           (edge ?cc "broader" ?pc ?r)]}
-                :settings {:relprod-strategy strategy}
-                :args [(d/db conn)
-                       rules
-                       (take 2 (the-concepts-per-type "ssyk-level-3"))
-                       (take 2 (the-concepts-per-type "ssyk-level-4"))]})))]
-        (println "Find all edges between a few small sets of concept types")
-        (print-strategy-times result-map)))))
+        (is (faster-strategy? result-map :expand-once :select-all))))))
