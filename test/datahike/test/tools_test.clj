b62e9c72502e90459eea1101e297d4c990111588
jonasseglare <5850088+jonasseglare@users.noreply.github.com>
2024-06-17 02:23:15 +0200

    Fix issue 676: make bind-by-fn require all attrs to have values (#677)
    * Fix issue 676: make bind-by-fn require all attrs to have values
    
    * FIXUP formatting

--- test/datahike/test/tools_test.clj
+++ test/datahike/test/tools_test.clj
@@ -20,3 +20,24 @@
            a (* a a)
            b (* b b)
            c (throw (ex-info "This element should not be evaluated" {}))))))
+(defn add-resolver [context [result-var a b]]
+  (when (and (contains? context a)
+             (contains? context b))
+    (assoc context result-var (+ (context a)
+                                 (context b)))))
+
+(deftest resolve-clauses-test
+  (is (= {:x 9 :y 10 :z 19 :w 28}
+         (dt/resolve-clauses add-resolver
+                             {:x 9 :y 10}
+                             [[:w :z :x]
+                              [:z :x :y]])))
+  (is (= {:x 9 :y 10 :z 19 :w 28}
+         (dt/resolve-clauses add-resolver
+                             {:x 9 :y 10}
+                             [[:z :x :y]
+                              [:w :z :x]])))
+  (is (thrown? Exception
+               (dt/resolve-clauses add-resolver
+                                   {:x 9 :y 10}
+                                   [[:w :z :x]]))))
