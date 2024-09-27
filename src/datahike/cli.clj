c88c3af8a85dfb8b0f38f63ea1c3567cee7a7c34
Coby Tamayo <acobster@users.noreply.github.com>
2024-09-27 10:35:30 -0700

    Use Timbre's default logging behavior in Babashka pod (#709)
    * test dthk stderr logging in run-native-image-tests
    
    * disable :println appender explicitly
    
    * only configure stderr logging outside Babashka
    
    ---------
    
    Co-authored-by: Coby Tamayo <coby@tamayo.email>

--- src/datahike/cli.clj
+++ src/datahike/cli.clj
@@ -16,12 +16,14 @@
 
 ;; This file is following https://github.com/clojure/tools.cli
 
-(log/merge-config!
- {:appenders {:println {:doc "Always prints to *err*"
-                        :enabled? true
-                        :fn (fn log-to-stderr [{:keys [output_]}]
-                              (binding [*out* *err*]
-                                (println (force output_))))}}})
+(when-not (= "true" (System/getenv "BABASHKA_POD"))
+  (log/merge-config!
+    {:appenders {:println {:enabled? false} ;; leave a "paper trail"
+                 :stderr {:doc "Always prints to *err*"
+                          :enabled? true
+                          :fn (fn log-to-stderr [{:keys [output_]}]
+                                (binding [*out* *err*]
+                                  (println (force output_))))}}}))
 
 (defn usage [options-summary]
   (->> [datahike-logo
