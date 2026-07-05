294db660e252e1e6868e0b3460648dbe899ca019
Haris <hello@harism.dev>
2026-07-05 11:12:21 +0900

    fix: remove :name gen-class fix in favor of build.clj compile fix

--- libdatahike/src/datahike/impl/libdatahike.clj
+++ libdatahike/src/datahike/impl/libdatahike.clj
@@ -8,7 +8,6 @@
   (:import [datahike.datom Datom]
            [datahike.connector Connection])
   (:gen-class
-   :name datahike.impl.libdatahike
    :methods [^{:static true} [parseJSON [String] Object]
              ^{:static true} [parseEdn [String] Object]
              ^{:static true} [parseCBOR [bytes] Object]
